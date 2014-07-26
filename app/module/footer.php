6a250bdfdf6993cf37be5fa768b37a342e06d670
prsolans <paul@aqdct.com>
2014-07-26 05:39:10 -0500

    Removed defer/async

--- app/module/footer.php
+++ app/module/footer.php
@@ -2,6 +2,6 @@
 </footer>
 <!-- /footer -->
 </div>
-<script src="js/angular.min.js" defer async></script>
-<script src="js/jquery-2.1.1.min.js" defer async></script>
-<script src="js/app.js" defer async></script>
+<script src="js/angular.min.js"></script>
+<script src="js/jquery-2.1.1.min.js"></script>
+<script src="js/app.js"></script>

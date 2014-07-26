6f46a1f6560735abe39040be311a327d25481fc1
prsolans <paul@aqdct.com>
2014-07-26 05:24:47 -0500

    Cleaned up menu routes, added async/defer to JS refs

--- app/module/header.php
+++ app/module/header.php
@@ -1,14 +1,15 @@
 <header id="header" class="nav">
     <ul>
-        <li><a href="index.php">Home</a>
+        <li><a href="/">Home</a>
         </li>
-        <li><a href="about.php">About</a>
+        <li><a href="about">About</a>
         </li>
-        <li><a href="projects.php">Projects</a>
+        <li><a href="projects">Projects</a>
         </li>
-        <li><a href="thoughts.php">Thoughts</a>
+        <li><a href="thoughts">Thoughts</a>
         </li>
-        <li><a href="contact.php">Contact</a>
+        <li><a href="contact">Contact</a>
         </li>
+        <li><a href="admin/web/app_dev.php/project">Admin</a></li>
     </ul>
 </header>
